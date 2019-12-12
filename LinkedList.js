39dcdcf0ae2fd9bb3c218a55c41b2429c6b6505c
maruihan <phenix_infinity@hotmail.com>
2019-12-12 17:05:14 +0800

    构建链表结构

--- LinkedList.js
+++ LinkedList.js
@@ -1,79 +1,52 @@
-class ListNode {
-  constructor(key) {
+/**
+ * 链表
+ * 链表的优点：
+    链接具有常量事件 插入和删除，因为我们可以只更改指针。
+    与数组一样，链表可以作为堆栈运行。
+ */
+function LinkedList() {
+  /**
+   * LinkedList数据结构还需要一个Node辅助类(行{1})。Node类表示要加入列表的项。它 3 包含一个element属性，即要添加到列表的值，以及一个next属性，即指向列表中下一个节点 项的指针。
+   * @param {*} element
+   */
+  let Node = function(element) {
+    this.element = element;
     this.next = null;
-    this.key = key;
-  }
+  };
+  let length = 0; // 存储列表项的数量(内部/私有变量)
+  let head = null; // 存储第一个节点的引用
+  /**
+   * 向列表尾部添加一个新的项。
+   */
+  this.append = function(element) {};
+  /**
+   * 向列表的特定位置插入一个新的项。
+   */
+  this.insert = function(position, element) {};
+  /**
+   * 从列表的特定位置移除一项。
+   */
+  this.removeAt = function(position) {};
+  /**
+   * 从列表中移除一项。
+   */
+  this.remove = function(element) {};
+  /**
+   * 返回元素在列表中的索引。如果列表中没有该元素则返回-1。
+   */
+  this.indexOf = function(element) {};
+  /**
+   * 如果链表中不包含任何元素，返回true，如果链表长度大于0则返回false。
+   */
+  this.isEmpty = function() {};
+  /**
+   * 返回链表包含的元素个数。与数组的length属性类似。
+   */
+  this.size = function() {};
+  this.getHead = function() {};
+  /**
+   * 由于列表项使用了Node类，就需要重写继承自JavaScript对象默认的toString方法，让其只输出元素的值。
+   */
+  this.toString = function() {};
+  this.print = function() {};
 }
-
-class List {
-  constructor() {
-    this.head = null;
-    this.length = 0;
-  }
-
-  static createNode(key) {
-    return new ListNode(key);
-  }
-
-  // 往头部插入数据
-  insert(node) {
-    // 如果有head 那么新node的next指向链表的head
-    if (this.head) {
-      node.next = this.head;
-    } else {
-      // 如果没有head 说明是最后一个 node的next指向null
-      node.next = null;
-    }
-    // 无论有没有数据 链表的head都指向新的node
-    this.head = node;
-    // 长度加一
-    this.length++;
-  }
-  // 从head开始查找
-  find(key) {
-    // 依次赋值链表中的node
-    let node = this.head;
-    // 查到值就返回值
-    while (node !== null && node.key !== key) {
-      node = node.next;
-    }
-    return node;
-  }
-
-  // 
-  delete(node) {
-    if (this.length === 0) {
-      throw "node is undefined";
-    }
-
-    if (node === this.head) {
-      this.head = node.next;
-      this.length--;
-      return;
-    }
-
-    let prevNode = this.head;
-
-    while (prevNode.next !== node) {
-      prevNode = prevNode.next;
-    }
-
-    if (node.next === null) {
-      prevNode.next = null;
-    }
-    if (node.next) {
-      prevNode.next = node.next;
-    }
-    this.length--;
-  }
-}
-const l = new List();
-const n1 = new ListNode(1);
-const n2 = new ListNode(2);
-const n3 = new ListNode(3);
-l.insert(n1);
-l.insert(n2);
-l.insert(n3);
-console.log(l);
-l.delete(n1)
-console.log(l);
--- index.js
+++ index.js
@@ -1460,4 +1460,64 @@ var target = 28;
 // };
 
 // console.log(twoSum(nums, target));
+/**
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
 
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+class ListNode {
+    constructor(val) {
+      this.next = null;
+      this.val = val;
+    }
+  }
+let l1 = [3, 5, 3];
+let l2 = [2, 7, 3];
+/**
+ *
+ * @param {*} l1
+ * @param {*} l2
+ */
+var addTwoNumbers = function(l1, l2) {
+  let result;
+  let cache = [];
+  let flag = 0;
+
+  while (l1 || l2) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + flag;
+    console.log(l1)
+
+    if (sum > 9) {
+      sum -= 10;
+      flag = 1;
+    } else {
+      flag = 0;
+    }
+
+    let list = new ListNode(sum);
+    if (cache[0]) {
+      cache[0].next = list;
+      cache[0] = list;
+    } else {
+      cache[0] = result = list;
+    }
+
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+
+  if (flag === 1) {
+    cache[0].next = new ListNode(1);
+  }
+  return result;
+};
+console.log(l1)
+
+console.log(addTwoNumbers(l1, l2))
