bd936dc1e26160e484c158764ec8ffd6af0b9ff1
maruihan <phenix_infinity@hotmail.com>
2019-12-26 23:21:26 +0800

    理解完成

--- es6.js
+++ es6.js
@@ -1,12 +1,10 @@
-function* helloWorldGenerator() {
-  yield 'hello';
-  yield 'world';
-  return 'ending';
+function* foo(x) {
+  var y = 2 * (yield x + 10);
+  var z = yield y / 4;
+  return x + y + z;
 }
+var a = foo(5);
 
-var hw = helloWorldGenerator();
-
-console.log(hw.next())
-console.log(hw.next())
-console.log(hw.next())
-console.log(hw.next())
+console.log(a.next()); // 第一次会执行yield括号内的
+console.log(a.next(4)); // 传值会重置上一次的yield的值 即是yield后面的数据
+console.log(a.next(3));
